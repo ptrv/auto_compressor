31666cc7a7c376013af10959a0645e90acff13cb
ptrv <mail@petervasil.net>
2014-03-17 23:15:30 +0100

    Add Xcode project files

--- JuceLibraryCode/modules/juce_audio_plugin_client/juce_audio_plugin_client.h
+++ JuceLibraryCode/modules/juce_audio_plugin_client/juce_audio_plugin_client.h
@@ -1,5 +1,11 @@
 // This is an auto-generated file to redirect any included
 // module headers to the correct external folder.
 
-#include "../../../juce/modules/juce_audio_plugin_client/juce_audio_plugin_client.h"
+#if defined (JUCER_LINUX_MAKE_7346DA2A)
+ #include "../../../juce/modules/juce_audio_plugin_client/juce_audio_plugin_client.h"
+#elif defined (JUCER_XCODE_MAC_F6D2F4CF)
+ #include "../../../../../juce/juce_audio_plugin_client/juce_audio_plugin_client.h"
+#else
+ #error "This file is designed to be used in an Introjucer-generated project!"
+#endif
 
