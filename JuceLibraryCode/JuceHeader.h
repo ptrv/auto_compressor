bfa50c98f5abc4b5e3e4dcb7c5974afec06369ca
ptrv <mail@petervasil.net>
2014-03-20 00:19:01 +0100

    Set version

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -34,8 +34,8 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "AutoCompressor";
-    const char* const  versionString  = "1.0.0";
-    const int          versionNumber  = 0x10000;
+    const char* const  versionString  = "0.1";
+    const int          versionNumber  = 0x100;
 }
 
 #endif   // __APPHEADERFILE_CD0JPL__
